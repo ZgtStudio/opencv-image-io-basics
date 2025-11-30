d0585dfabbdf0b400c92dde69a845c505250b18c
yigitmerterkalay <150773828+yigitmerterkalay@users.noreply.github.com>
2025-11-30 18:22:37 +0300

    main.py

--- OpenCV Image Reading, Displaying and Saving/main.py
+++ OpenCV Image Reading, Displaying and Saving/main.py
@@ -3,8 +3,9 @@ import numpy as np # importing Numpy and assign to np
 
 image = cv2.imread("cameraman.tif", 0) # read the cameraman image in grayscale mode
 cv2.imshow("Cameraman", image) # show the image in a window titled
-cv2.imwrite("new_black-white_image.png", iamge) # save the new image in the same path
+cv2.imwrite("new_black-white_image.png", image) # save the new image in the same path
 
 cv2.waitKey(0) # close with any key
 
 cv2.destroyAllWindows() # close all image windows
+
